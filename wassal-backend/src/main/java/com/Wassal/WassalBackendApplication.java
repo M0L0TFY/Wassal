4dbfa5eefd019a53f83c2d7f95eb2d2ccb3f69ce
Mohamed Lotfy <moh.lotfy05@gmail.com>
2025-07-03 19:44:15 +0300

    Project backend setup

---
+++ wassal-backend/src/main/java/com/Wassal/WassalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Wassal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WassalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WassalBackendApplication.class, args);
+	}
+
+}
